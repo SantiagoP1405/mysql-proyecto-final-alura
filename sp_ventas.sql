ee4fe0a8443e77c5c5e3d4cec2c2fc7cf280f0e3
SantiagoP1405 <116893930+SantiagoP1405@users.noreply.github.com>
2025-04-23 12:32:15 -0600

    VERSIÓN 2. GESTIÓN DE VENTAS ALEATORIAS
    - Creación de un ciclo WHILE para iterar según el número máximo de ítems.
    - Generación de un producto aleatorio y una cantidad aleatoria para cada iteración.
    - Selección del precio del producto desde la tabla de productos.
    - Inserción de los datos generados en la tabla ítems.
    - Se identificó un error en las claves primarias que se corregirppa en versiones posteriores

--- sp_ventas.sql
+++ sp_ventas.sql
@@ -17,15 +17,25 @@ DECLARE vcantidad INT;
 DECLARE vprecio FLOAT;
 DECLARE vitems INT;
 DECLARE vnfactura INT;
-
+DECLARE vcontador INT DEFAULT 1;
 SELECT MAX(NUMERO) + 1 INTO vnfactura FROM facturas;
 SET vcliente = f_cliente_aleatorio();
 SET vvendedor = f_vendedor_aleatorio();
-INSERT INTO facutras(NUMERO, FECHA, DNI, MATRICULA, IMPUESTO)
+INSERT INTO facturas(NUMERO, FECHA, DNI, MATRICULA, IMPUESTO)
 VALUES(vnfactura, fecha, vcliente, vvendedor, 0.16);
 SET vitems = f_aleatorio(1, maxitems);
-
+WHILE vcontador <= vitems
+DO
+SET vproducto = f_producto_aleatorio();
+SET vcantidad = faleatorio(1, maxcantidad);
+SELECT PRECIO INTO vprecio FROM productos WHERE CODIGO = vproducto;
+INSERT INTO items(NUMMERO, CODIGO, CANTIDAD, PRECIO)
+VALUES(vnfactura, vproducto, vcaantidad, vprecio);
+SET vcontador = vcontador +1;
+END WHILE;
 END$$
 
 DELIMITER ;
+
+CALL sp_venta(20250423, 15, 100);
 
